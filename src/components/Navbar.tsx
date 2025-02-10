1839645fff13dc8a10bc4993ba23b0b484642a58
Kekeli Afantchao <kelkiiii@mithrandir.local>
2025-02-10 10:49:33 -0500

    installed prettier and eslint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="flex justify-between items-center p-4 bg-gray-900 text-white">
+      <a href="#" className="text-lg font-bold">Brand</a>
+      <ul className="flex space-x-4">
+      <li><a href="#" className="hover:text-gray-300">Albums/EPs</a></li>
+      <li><a href="#" className="hover:text-gray-300">Singles</a></li>
+      <li><a href="#" className="hover:text-gray-300">Login</a></li>
+  </ul>
+</nav>
   )
 }
