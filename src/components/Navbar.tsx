5fb938d480aa82cb64dd28f3e63c39a862037f5c
Kekeli Afantchao <kelkiiii@mithrandir.local>
2025-02-10 10:51:39 -0500

    removed deprecated .eslintignore

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-900 text-white">
-      <a href="#" className="text-lg font-bold">Brand</a>
+      <a href="#" className="text-lg font-bold">
+        Brand
+      </a>
       <ul className="flex space-x-4">
-      <li><a href="#" className="hover:text-gray-300">Albums/EPs</a></li>
-      <li><a href="#" className="hover:text-gray-300">Singles</a></li>
-      <li><a href="#" className="hover:text-gray-300">Login</a></li>
-  </ul>
-</nav>
-  )
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Albums/EPs
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Singles
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-300">
+            Login
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
